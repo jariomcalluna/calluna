a494a03e748754d81b165f07c6f5d2ec94b868d1
jariomcalluna <93946339+jariomcalluna@users.noreply.github.com>
2021-11-08 21:41:39 +0100

    Add files via upload

---
+++ js/defaultscripts.js
@@ -0,0 +1,5 @@
+<!--
+function open_window(url, w, h) {
+	window.open(url,"_",'top=100,left=50,alwaysRaised,dependent,toolbar=0,location=0,directories=0,status=0,menubar=0,scrollbars=yes,resizable=0,width=' + w + ',height=' + h);
+}
+ -->
